ebeeb8b1672eff5bde57a9e6c33d90f208e2808b
Avinash5656 <110724106+Avinash5656@users.noreply.github.com>
2023-08-06 14:52:13 +0530

    added fetures

--- src/component/todo.js
+++ src/component/todo.js
@@ -1,26 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import swal from 'sweetalert';
 import '../App.css';
 
 const Todo = () => {
 
   const [inputdata, setInputdata] = useState('');
-  const [items, setItems] = useState([]);
-
-
+  const [items, setItems] = useState(() => {
+    const storedItems = localStorage.getItem('todo_items');
+    return storedItems ? JSON.parse(storedItems) : [];
+  });
   // adding a task
   const additem = () => {
     if (!inputdata) {
       swal("Add Something", "To your task list", "info")
-
     }
     else {
       setItems([...items, inputdata])
       setInputdata('')
 
     }
-
-
   }
   // deleting a task
   const deleteitem = (id) => {
@@ -33,11 +31,32 @@ const Todo = () => {
   }
 
   // removing all
-
   const removeall = () => {
     setItems([]);
   }
 
+   // Function to load data from local storage on initial render
+   useEffect(() => {
+    const storedItems = localStorage.getItem('todo_items');
+    if (storedItems) {
+      setItems(JSON.parse(storedItems));
+    }
+  }, []);
+
+  // Function to save data to local storage whenever items change
+  useEffect(() => {
+    localStorage.setItem('todo_items', JSON.stringify(items));
+  }, [items]);
+  
+  // on pressing enter adds item
+  const check = (e)=>{
+    if (e.key === 'Enter'){
+      additem();
+    }
+
+  }
+
+ 
 
 
   return (
@@ -46,13 +65,14 @@ const Todo = () => {
         <div className='child-div'>
 
 
-          <h2>Add your task's below</h2>
+          <h2 className='heading'>Task Planner</h2>
           <div className='additems'>
-            <input type="text" placeholder='Add here' className='inputbox'
+            <input type="text" placeholder='Type something' className='inputbox'
               value={inputdata}
               onChange={(e) => setInputdata(e.target.value)}
+              onKeyUp={check}
             />
-            <i className="fa-duotone fa-plus fonticon" title='add item' onClick={additem} ></i>
+            <div><button onClick={additem} className='add-btn'>Add</button></div>
           </div>
 
 
@@ -63,7 +83,7 @@ const Todo = () => {
                 return (
                   <div className='each-item' key={index}>
                     <h2 className='show-item'>{elem}</h2>
-                    <i className="fa-solid fa-trash delicon" title='delte item' onClick={() => deleteitem(index)}></i>
+                    <i className="fa-solid fa-trash delicon trash-can" title='delte item' onClick={() => deleteitem(index)} ></i>
 
                   </div>
 
